6501292f4b19d8db7b1ab1fd6314a419cd793d91
Cilibeanu Sebastian <cilibeanu.nicolae@iisgalvanimi.edu.it>
2023-05-28 15:54:31 +0000

    apparizione tane nella mappa

---
+++ pythonrecupero/main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+import geojson
+app = Flask(__name__)
+
+# Stringa di connessione al DB
+app.config["MONGO_URI"] = "mongodb+srv://S3ba26:Facebook2015@cluster0.ztaqymo.mongodb.net/pokemon" #Importante qui va specificato il nome del DB
+
+mongo = PyMongo(app)
+# Per rispondere alle chiamate cross origin
+CORS(app)
+
+@app.route("/all")
+def get_all():
+    tane = mongo.db.tane
+    output = [] 
+    for s in tane.find().limit(100):
+        output.append({"lat": s['lat'], "lng":s['lng']})
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run()
+  
+
