95fb74713053f09a2e34517dde8c1d4640190774
Cilibeanu Sebastian <cilibeanu.nicolae@iisgalvanimi.edu.it>
2023-05-28 17:53:24 +0000

    apparizione coordinate casuali pikachu,charmender , bulbasaur, snorlax

--- angular-recupero/src/app/app.component.ts
+++ angular-recupero/src/app/app.component.ts
@@ -49,6 +49,71 @@ export class AppComponent implements OnInit {
   mostra() {
     this.accendi = !this.accendi
   }
+    //richiesta http di una cordinata random
+    mostrapikachu() {
+      this.markers = [];
+      this.http.get<Prova>(this.url + "pikachu").subscribe(data => {
+        let lng = data["lng"]
+        let lat = data["lat"]
+        let marker: Marker = new Marker(lat, lng);
+        this.markers.push(marker)
+  
+        //immagine
+        let iconData: google.maps.Icon = {
+          url: '/assets/img/pikachu.ico',
+          scaledSize: new google.maps.Size(60, 60)
+        }
+        this.markerOptions = { icon: iconData }
+      })
+    }
+  
+    mostrabulbasaur() {
+      this.markers = [];
+      this.http.get<Prova>(this.url + "bulbasaur").subscribe(data => {
+        let lng = data["lng"]
+        let lat = data["lat"]
+        let marker: Marker = new Marker(lat, lng);
+        this.markers.push(marker)
+  
+        let iconData: google.maps.Icon = {
+          url: 'assets/img/bulbasaur.ico',
+          scaledSize: new google.maps.Size(60, 60)
+        }
+        this.markerOptions = { icon: iconData }
+      })
+    }
+  
+    mostracharmender() {
+      this.markers = [];
+      this.http.get<Prova>(this.url + "charmender").subscribe(data => {
+        let lng = data["lng"]
+        let lat = data["lat"]
+        let marker: Marker = new Marker(lat, lng);
+        this.markers.push(marker)
+  
+        let iconData: google.maps.Icon = {
+          url: '/assets/img/charmender.ico',
+          scaledSize: new google.maps.Size(60, 60)
+        }
+        this.markerOptions = { icon: iconData }
+      })
+    }
+  
+    mostrasnorlax() {
+      this.markers = [];
+      this.http.get<Prova>(this.url + "snorlax").subscribe(data => {
+        let lng = data["lng"]
+        let lat = data["lat"]
+        let marker: Marker = new Marker(lat, lng);
+        this.markers.push(marker)
+  
+        let iconData: google.maps.Icon = {
+          url: '/assets/img/snorlax.ico',
+          scaledSize: new google.maps.Size(60, 60)
+        }
+        this.markerOptions = { icon: iconData }
+      })
+    }
   
 }
 
